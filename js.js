628afd3939e704dd2b725d420fb5a5d3e8ee8bb7
lana <lanalezhava4@gmail.com>
2025-03-23 12:03:06 +0100

    fir

--- js.js
+++ js.js
@@ -126,6 +126,8 @@ videoBtns.addEventListener('click', function () {
 const slider = document.querySelectorAll('.slider')
 const next = document.querySelector('.next')
 const prev = document.querySelector('.prev')
+
+// .........
 slider.forEach(function (sliders, index) {
     sliders.style.left = `${index * 100}%`
 })
@@ -154,3 +156,250 @@ function disPlay() {
         e.style.transform = `translateX(-${counts * 100}%)`
     })
 }
+const right = document.querySelector('.right')
+const left = document.querySelector('.left')
+const frameSlider = document.querySelector('.frame-slider')
+const imge = document.querySelectorAll('.imge')
+const bottom = document.querySelector('.bottom')
+slideNumber = 0
+for (let i = 0; i < imge.length; i++) {
+    const div = document.createElement('div')
+    div.className = 'button'
+    bottom.appendChild(div)
+}
+const button = document.querySelectorAll('.button')
+button[0].style.backgroundColor = '#1f7ea9'
+const bgCol = () => {
+    button.forEach((tranSp) => {
+        tranSp.style.backgroundColor = 'transparent'
+    })
+}
+const changDivCol = () => {
+    button.forEach(function (div, i) {
+        div.addEventListener('click', () => {
+            bgCol()
+            frameSlider.style.transform = `translateX(-${i * 60}rem)`;
+            div.style.backgroundColor = '#1f7ea9'
+        })
+
+    })
+}
+
+changDivCol()
+right.addEventListener('click', () => {
+    if (slideNumber < imge.length - 1) {
+        nextSlid()
+        bgCol()
+        button[slideNumber].style.backgroundColor = '#1f7ea9'
+    } else {
+        getFirstSlider()
+    }
+    // dissPlay()
+});
+
+left.addEventListener('click', () => {
+    if (slideNumber >= 1) {
+        prevSlider()
+        bgCol()
+        button[slideNumber].style.backgroundColor = '#1f7ea9'
+
+
+    } else {
+        getFirstSlider()
+    }
+    // dissPlay()
+})
+// const dissPlay = () => {
+//     if (slideNumber < imge.length - 1) {
+//         right.style.display = 'block'
+//     } else {
+//         right.style.display = 'none'
+//     } if (slideNumber > 0) {
+//         left.style.display = 'block'
+//     } else {
+//         left.style.display = 'none'
+//     }
+// }
+const nextSlid = () => {
+    slideNumber++;
+    frameSlider.style.transform = `translateX(-${slideNumber * 60}rem)`;
+}
+const prevSlider = () => {
+    frameSlider.style.transform = `translateX(-${(slideNumber - 1) * 60}rem)`
+    slideNumber--;
+}
+const getFirstSlider = () => {
+    slideNumber = 0; // Reset first
+    frameSlider.style.transform = `translateX(0px)`;
+}
+// checkbox-Toggle
+const inputEl = document.querySelector('.input')
+const checkToggle = document.querySelector('.checkbox-Toggle')
+inputEl.checked = JSON.parse(localStorage.getItem('mode'))
+updateBody()
+function updateBody() {
+    if (inputEl.checked) {
+        checkToggle.style.backgroundColor = '#2291c4'
+    } else {
+        checkToggle.style.backgroundColor = '#ffffff'
+    }
+}
+inputEl.addEventListener('input', () => {
+    updateBody()
+    updateLocalStirage()
+})
+function updateLocalStirage() {
+    localStorage.setItem('mode', JSON.stringify(inputEl.checked))
+}
+// testimonials
+const testimonials = [
+    {
+        name: 'Lana Lezhava',
+        text: ' adipisicing elit. Similique expedita blanditiis vitae voluptatem ea corporis, facilis sapiente, rerum fugit accusamus, molestiae esse. Quasi dolorum dolorem maxime dicta architecto, culpa ducimus?',
+    },
+    {
+        name: 'Diana Ashba',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque minima sapiente perspiciatis quam. Quia quibusdam magni quisquam adipisci, facere recusandae!',
+    },
+    {
+        name: 'Mari gangad',
+        text: ', dolor sit amet consectetur adipisicing elit. Vel, perferendis fugit. Modi, itaque. Nesciunt rerum corporis provident porro iste mollitia nihil rem totam, consequatur error, ad qui voluptates enim sint!',
+    }
+]
+let idx = 0
+const testiText = document.querySelector('.testi-text')
+const testiName = document.querySelector('.testi-name')
+function updateTestimonials() {
+    const { name, text } =
+        testimonials[idx]
+    testiText.innerText = text
+    testiName.innerText = name
+    idx++
+    if (idx === testimonials.length) {
+        idx = 0
+    }
+    setTimeout(() => {
+        updateTestimonials()
+    }, 5000)
+}
+updateTestimonials()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// flex-container
+// const right = document.querySelector('.right')
+// const left = document.querySelector('.left')
+// const frameSlider = document.querySelector('.frame-slider')
+// const imge = document.querySelectorAll('.imge')
+// const bottom = document.querySelector('.bottom')
+// slideNumber = 1
+// for (let i = 0; i < imge.length; i++) {
+//     const div = document.createElement('div');
+//     div.className = 'button';
+//     bottom.appendChild(div);
+// }
+
+// const buttons = document.querySelectorAll('.button'); // Get all created buttons
+// buttons[0].style.backgroundColor = '#1f7ea9'; // Set first button to black
+
+// const resetBg = () => {
+//     buttons.forEach((btn) => {
+//         btn.style.backgroundColor = 'transparent'
+//     })
+// }
+// const change = function () {
+//     buttons.forEach(function (btn, i) {
+//         btn.addEventListener('click', () => {
+//             resetBg()
+//             // Move the slider
+//             frameSlider.style.transform = `translateX(-${i * 60}rem)`;
+
+//             // Reset all button colors
+//             btn.style.backgroundColor = '#1f7ea9';
+//         });
+//     });
+// };
+
+// change();
+// right.addEventListener('click', function () {
+//     if (slideNumber < imge.length) {
+//         frameSlider.style.transform = `translateX(-${slideNumber * 60}rem)`
+//         slideNumber++
+
+//     } else {
+//         frameSlider.style.transform = `translateX(0px)`
+//         slideNumber = 0
+//     }
+//     resetBg()
+//     // buttons[slideNumber - 1].style.backgroundColor = '#1f7ea9';
+//     if (slideNumber > 0) { // Prevent accessing index -1
+//         buttons[slideNumber - 1].style.backgroundColor = '#1f7ea9';
+//     }
+// })
+// left.addEventListener('click', () => {
+//     if (slideNumber > 0) {
+//         frameSlider.style.transform = `translateX(-${(slideNumber - 2) * 60}rem)`
+//         slideNumber--
+//         resetBg()
+//         buttons[slideNumber - 1].style.backgroundColor = '#1f7ea9';
+//     }
+
+// })
+
+
+// const disp = () => {
+//     if (slideNumber < buttons.length - 1) {
+//         right.style.display = 'block'
+//     } else {
+//         right.style.display = 'none'
+//     }
+// };
+
+
+
